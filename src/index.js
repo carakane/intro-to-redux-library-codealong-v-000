4f87191a7ce22e7cf9df601d3cf25d8b8b1a2562
Cara Kane <cara.kane@gmail.com>
2017-10-01 18:38:22 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import shoppingListItemReducer from 'src/reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); /* code change */
+const store = createStore(shoppingListItemReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); /* code change */
 
 ReactDOM.render(
-  <App store={store} />, 
+  <App store={store} />,
   document.getElementById('root')
 );
