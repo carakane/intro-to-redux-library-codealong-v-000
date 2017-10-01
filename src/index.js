3e68994abb7104db3cc55fa2213c9f05463d3683
Cara Kane <cara.kane@gmail.com>
2017-10-01 18:35:17 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; 
+import { createStore } from 'redux';
 import shoppingListItemReducer from 'src/reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
- 
-const store = createStore(shoppingListItemReducer); 
- 
+
+const store = createStore(shoppingListItemReducer);
+
 ReactDOM.render(
   <App store={store} />, 
   document.getElementById('root')
